0a1220b34982cc505286c10c5a11debb4ff74292
tsumo <tsumo@users.noreply.github.com>
2020-01-10 15:52:36 +0300

    Adding mobx-crash-course build

---
+++ mobx-crash-course/precache-manifest.ae1757fce3feacde82184146239426a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bab23462f54a51cd4a61fa9d340d7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "409792088a9b245b406d",
+    "url": "/static/css/main.f92e4e2e.chunk.css"
+  },
+  {
+    "revision": "62ea09a7c64c95221618",
+    "url": "/static/js/2.dffb58a7.chunk.js"
+  },
+  {
+    "revision": "69acc368311a28b74c256d4aa3e0e0ae",
+    "url": "/static/js/2.dffb58a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "409792088a9b245b406d",
+    "url": "/static/js/main.5922020a.chunk.js"
+  },
+  {
+    "revision": "a852ed467e6de944bae4",
+    "url": "/static/js/runtime-main.e5fb301a.js"
+  }
+]);
